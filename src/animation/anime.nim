aae139923d0905ffd7c4fb48a0c960d7a282b530
Oloqq <olgierdpk@gmail.com>
2022-09-09 21:28:03 +0200

    multisequence

--- src/animation/anime.nim
+++ src/animation/anime.nim
@@ -1,43 +1,52 @@
 import boxy, windy
+import std/tables
 
 type
+    Animations* = object
+        current: IVec2
+        frameSequences: Table[string, IVec2]
     Anime = ref object
         boxy: Boxy
-        maxi: uint
-        cur: uint
+        frame: int
         code: string
+        animations: Animations
 
+proc newAnimations*(framesSequences: Table[string, IVec2], starting: string): Animations =
+    result.frameSequences = framesSequences
+    result.current = framesSequences[starting]
 
-proc newAnime*(boxy: Boxy, sheet: Image, frame_size: IVec2): Anime =
+proc newAnime*(boxy: Boxy, sheet: Image, frameSize: IVec2, animations: Animations, uniqKey: string): Anime =
     result = new Anime
     result.boxy = boxy
-    result.code = "bruh"
+    result.code = uniqKey
+    result.animations = animations
 
     var
         x = 0
         y = 0
-        i = 0u
+        i = 0
 
     while y < sheet.height:
         while x < sheet.width:
-            boxy.addImage(result.code & $i, subImage(sheet, x, y, frame_size.x, frame_size.y))
+            boxy.addImage(result.code & $i, subImage(sheet, x, y, frameSize.x, frameSize.y))
             inc i
-            x += frame_size.x
+            x += frameSize.x
         x = 0
-        y += frame_size.y
+        y += frameSize.y
 
-    result.maxi = i-1
+proc newAnime*(boxy: Boxy, source: string, frameSize: IVec2, animations: Animations, uniqKey: string): Anime =
+    return newAnime(boxy, readImage(source), frameSize, animations, uniqKey)
 
-
+proc newAnime*(boxy: Boxy, source: string, frameSize: IVec2, animations: Animations): Anime =
+    return newAnime(boxy, readImage(source), frameSize, animations, source)
 
 proc draw*(bxy: Boxy, anim: Anime) =
-    # bxy.drawRect(rect(50, 50, 100, 100), color(100, 100, 100))
-    # bxy.drawImage("bruh", pos=vec2(0, 0))
-    drawImage(bxy, anim.code & $anim.cur, pos=vec2(0, 0))
-    inc anim.cur
-    if anim.cur >= anim.maxi:
-        anim.cur = 0
+    drawImage(bxy, anim.code & $anim.frame, pos=vec2(0, 0))
+    inc anim.frame
+    if anim.frame >= anim.animations.current[1]:
+        anim.frame = anim.animations.current[0]
 
 
 proc sampleAnime*(bxy: Boxy, window: Window): Anime =
-    return newAnime(bxy, readImage("data/img/anime.png"), ivec2(32, 32))
+    let animations = newAnimations({ "idle": ivec2(0, 20), "death": ivec2(90, 100) }.toTable, "death")
+    return newAnime(bxy, "data/img/anime.png", ivec2(32, 32), animations)
