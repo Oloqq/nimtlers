72f9891618951f221f73ea264588eb5e84b06026
Oloqq <olgierdpk@gmail.com>
2022-09-09 19:00:57 +0200

    pretty main

--- src/nimtlers.nim
+++ src/nimtlers.nim
@@ -2,39 +2,26 @@ import boxy, opengl, windy
 import map/hexmap
 
 let windowSize = ivec2(1280, 800)
-
 let window = newWindow("Windy + Boxy", windowSize)
 makeContextCurrent(window)
-
 loadExtensions()
 
 let bxy = newBoxy()
 var map = newHexmap(ivec2(4, 5))
 map.render(bxy)
 
-# Load the images.
-# bxy.addImage("bg", readImage("data/img/cat.jpg"))
-
-var frame: int
-
-# Called when it is time to draw a new frame.
-proc display() =
-  # Clear the screen and begin a new frame.
+proc draw() =
   bxy.beginFrame(windowSize)
 
-  # bxy.drawImage("bg", rect = rect(vec2(0, 0), windowSize.vec2))
-
   map.draw(bxy)
-  # bxy.drawImage(HEXES_DEBUG, pos = vec2(0, 0))
 
-  # End this frame, flushing the draw commands.
   bxy.endFrame()
-  # Swap buffers displaying the new Boxy frame.
   window.swapBuffers()
-  inc frame
 
+proc main() =
+  while not window.closeRequested:
+    draw()
+    pollEvents()
 
-
-while not window.closeRequested:
-  display()
-  pollEvents()
+when isMainModule:
+  main()
